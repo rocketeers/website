13db9a751794b510a3aa647e3bdd19a198649e69
Maxime Fabre <ehtnam6@gmail.com>
2014-08-16 16:04:51 +0200

    Update Rocketeer configuration

---
+++ .rocketeer/strategies.php
@@ -0,0 +1,43 @@
+<?php
+use Rocketeer\Binaries\Composer;
+use Rocketeer\Tasks\Subtasks\Primer;
+
+return array(
+
+	// Task strategies
+	//
+	// Here you can configure in a modular way which tasks to use to
+	// execute various core parts of your deployment's flow
+	//////////////////////////////////////////////////////////////////////
+
+	// Which strategy to use to create a new release
+	'deploy'       => 'Clone',
+
+	// Which strategy to use to test your application
+	'test'         => 'Phpunit',
+
+	// Which strategy to use to migrate your database
+	'migrate'      => 'Artisan',
+
+	// Which strategy to use to install your application's dependencies
+	'dependencies' => 'Polyglot',
+
+	// Execution hooks
+	//////////////////////////////////////////////////////////////////////
+
+	'composer' => array(
+		'install' => function (Composer $composer, $task) {
+			return $composer->install([], ['--no-interaction' => null, '--no-dev' => null, '--prefer-dist' => null]);
+		},
+		'update' => function (Composer $composer) {
+			return $composer->update();
+		},
+	),
+
+	'primer' => function (Primer $task) {
+		return array(
+			//$task->binary('grunt')->run('lint'),
+		);
+	}
+
+);
