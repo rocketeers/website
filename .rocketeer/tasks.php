2735edd58e2fc1e6539f6ec61b7ca4e87122e3b6
Maxime Fabre <ehtnam6@gmail.com>
2014-08-16 16:28:07 +0200

    Fix task

--- .rocketeer/tasks.php
+++ .rocketeer/tasks.php
@@ -1,6 +1,6 @@
 <?php
 use Rocketeer\Facades\Rocketeer;
 
-Rocketeer::task('grunt', 'node_modules/.bin/grunt production', 'Build the assets and archives');
+Rocketeer::task('grunt', 'node_modules/.bin/grunt production --force', 'Build the assets and archives');
 
 Rocketeer::listenTo('deploy.before-symlink', 'grunt');
