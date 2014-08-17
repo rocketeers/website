7cd19f18643e635dbab959a107b4939afb18b403
Maxime Fabre <ehtnam6@gmail.com>
2014-08-17 02:57:36 +0200

    Update SCM branch

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -8,7 +8,7 @@
 
 	// The SSH/HTTPS address to your repository
 	// Example: https://github.com/vendor/website.git
-	'repository' => 'https://github.com/Anahkiasen/rocketeer.git',
+	'repository' => 'https://github.com/rocketeers/website.git',
 
 	// The repository credentials : you can leave those empty
 	// if you're using SSH or if your repository is public
