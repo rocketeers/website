4f3458094f6f1aba11d61eea1036b71761112c96
Maxime Fabre <ehtnam6@gmail.com>
2014-08-22 23:59:53 +0200

    Slight cleanup

--- src/DocumentationGatherer.php
+++ src/DocumentationGatherer.php
@@ -2,45 +2,63 @@
 namespace Rocketeer\Website;
 
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class DocumentationGatherer
 {
+	/**
+	 * @type array
+	 */
+	protected $documentation = [];
+
 	/**
 	 * @return array
 	 */
 	public function getDocumentation()
 	{
-		$documentation = [];
-
 		// Gather files
 		$files = new Finder();
 		$files = $files->in(__DIR__.'/../docs/*')->name('*.md')->files();
-
-		/** @type \SplFileObject[] $files */
 		foreach ($files as $file) {
-			$folder   = $file->getPath();
-			$folder   = basename($folder);
-			$category = preg_replace('/[IV]+-(.+)/', '$1', $folder);
-
-			// Create documentation entry the first time
-			if (!isset($documentation[$category])) {
-				$documentation[$category] = array(
-					'label' => $category,
-					'pages' => [],
-				);
-			}
-
-			// Build handle and label
-			$name   = $file->getBasename('.md');
-			$handle = str_replace('-', ' ', $name);
-
-			$documentation[$category]['pages'][$handle] = 'docs/'.$folder.'/'.$name;
+			$this->addPage($file);
 		}
 
 		// Add custom pages
-		$documentation['Introduction']['pages'] = ['Introduction' => 'README'] + $documentation['Introduction']['pages'];
-		$documentation['Help']['pages']         = ['Changelog' => 'CHANGELOG'] + $documentation['Help']['pages'];
+		$this->addCustomPages();
 
-		return array_values($documentation);
+		return array_values($this->documentation);
+	}
+
+	/**
+	 * @param SplFileInfo $file
+	 */
+	protected function addPage(SplFileInfo $file)
+	{
+		$folder   = $file->getPath();
+		$folder   = basename($folder);
+		$category = preg_replace('/[IV]+-(.+)/', '$1', $folder);
+
+		// Create documentation entry the first time
+		if (!isset($this->documentation[$category])) {
+			$this->documentation[$category] = array(
+				'label' => $category,
+				'pages' => [],
+			);
+		}
+
+		// Build handle and label
+		$name   = $file->getBasename('.md');
+		$handle = str_replace('-', ' ', $name);
+
+		$this->documentation[$category]['pages'][$handle] = 'docs/'.$folder.'/'.$name;
+	}
+
+	/**
+	 * Add the custom pages to the documentations
+	 */
+	protected function addCustomPages()
+	{
+		$this->documentation['Introduction']['pages'] = ['Introduction' => 'README'] + $this->documentation['Introduction']['pages'];
+		$this->documentation['Help']['pages']         = ['Changelog' => 'CHANGELOG'] + $this->documentation['Help']['pages'];
 	}
 }
