671182860a5d6c4421c8168afb007ec069e6e935
Maxime Fabre <ehtnam6@gmail.com>
2014-09-17 21:50:06 +0200

    Tweak to tasks

--- .rocketeer/tasks.php
+++ .rocketeer/tasks.php
@@ -4,16 +4,28 @@ use Rocketeer\Facades\Rocketeer;
 // Tasks
 //////////////////////////////////////////////////////////////////////
 
-Rocketeer::task('grunt', 'node_modules/.bin/grunt production --force', 'Build the assets and archives');
+Rocketeer::task(
+	'grunt',
+	'node_modules/.bin/grunt production --force',
+	'Build the assets and archives'
+);
 
-Rocketeer::task('phar', [
-	'cd docs/rocketeer',
-	'composer install',
-	'php bin/compile',
-	'mv bin/rocketeer.phar ../../public/versions/rocketeer.phar',
-], 'Generates the final PHAR');
+Rocketeer::task(
+	'phar',
+	array(
+		'cd docs/rocketeer',
+		'composer install',
+		'php bin/compile',
+		'mv bin/rocketeer.phar ../../public/versions/rocketeer.phar',
+	),
+	'Generates the final PHAR'
+);
 
-Rocketeer::task('phpdoc', 'vendor/bin/phpdoc -t public/api -d docs/rocketeer/src', 'Generates the API documentation');
+Rocketeer::task(
+	'phpdoc',
+	'vendor/bin/phpdoc -t public/api -d docs/rocketeer/src',
+	'Generates the API documentation'
+);
 
 // Events
 //////////////////////////////////////////////////////////////////////
