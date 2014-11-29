773926312cb3b9347f66b2179eb3942d0a39fd86
Maxime Fabre <ehtnam6@gmail.com>
2014-11-29 17:26:42 +0100

    Add Satellite PHAR generation

--- .grunt/development/shell.js
+++ .grunt/development/shell.js
@@ -1,10 +1,17 @@
 module.exports = {
 	phar: {
 		command: [
+			// Generate Rocketeer PHAR
 			'cd docs/rocketeer',
 			'composer install',
 			'php bin/compile',
 			'mv bin/rocketeer.phar ../../public/versions/rocketeer.phar',
+
+			// Generate Satellite PHAR
+			'cd docs/satellite',
+			'composer install',
+			'php bin/compile',
+			'mv bin/satellite.phar ../../public/versions/satellite.phar',
 		].join('&&'),
 	},
 
