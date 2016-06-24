5f80f597408fc231b32a78e16571285a080b3696
Maxime Fabre <ehtnam6@gmail.com>
2016-06-25 00:06:44 +0200

    Work on Gitbook conversion

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Create stylesheets
+npm run build:assets
+rm -rf docs/docs/builds
+mv public/builds docs/docs/builds
+
+# Copy necessary source files
+rm -rf docs/docs/_layouts
+cp -r _layouts docs/docs/_layouts
+cp docs/rocketeer/*.md docs/docs
+
+# Create Gitbook and copy assets
+npm run build:gitbook
+cp -r _book/* public
+cp public/favicon.png public/gitbook/images/favicon.ico
+
+# Cleanup docs folder
+cd docs/docs
+git reset --hard
+git clean -df
