4b9ded2b8d36672f93abf9432124a789194920e3
Maxime Fabre <ehtnam6@gmail.com>
2016-06-24 16:51:30 +0200

    Fix production build

--- public/index.php
+++ public/index.php
@@ -7,7 +7,9 @@ include __DIR__.'/../vendor/autoload.php';
 
 $app = new Application();
 $docs = new DocumentationGatherer();
+$assets = json_decode(file_get_contents(__DIR__.'/builds/manifest.json'), true);
 
 return $app->view->display('home.twig', [
     'docs' => $docs->getDocumentation(),
+    'assets' => $assets,
 ]);
